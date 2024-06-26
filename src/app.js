6b90a27f422c7ff07723ec7ed3c3331502bf0d47
Avinash Kumar <avinash.k95347@gmail.com>
2024-06-26 10:38:22 +0530

    Add authentication middleware to '/me' route in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-
+import { verifyJwt } from './middlewares/auth.middleware.js';
 
 const app = express();
 
@@ -15,13 +15,14 @@ app.use(cors({origin:process.env.CORS_ORIGIN, credentials:true}));
 // Routes import
 import {userRouter} from './routes/user.routes.js';
 import {qsRouter} from './routes/qs.routes.js';
+import { getMyProfile } from './controllers/user.controller.js';
 
 
 app.get('/',(req,res)=>{
     res.send('hello world!!')
 })
 // Routes
+app.use('/me',verifyJwt,getMyProfile);
 app.use('/user', userRouter);
 app.use('/qs', qsRouter);
-
 export {app};
