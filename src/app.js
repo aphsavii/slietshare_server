d97269dabd44f5d8073f1a8f626e1b2b0ee29ba5
Avinash Kumar <avinash.k95347@gmail.com>
2024-08-19 15:55:32 +0530

    fix cors

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(express.json({limit:'16kb'}));
 app.use(express.urlencoded({ extended: true, limit:'16kb' }));
 app.use(cookieParser());
 app.use(express.static('public'));
-app.use(cors({origin:process.env.CORS_ORIGIN, credentials:true}));
+app.use(cors( {origin: true, credentials: true} ));  
 
 io.on('connection',(socket)=>{
     console.log('user connected',socket.id,"->",socket.user.regno);
